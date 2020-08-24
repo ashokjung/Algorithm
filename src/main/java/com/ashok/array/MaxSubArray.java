41b14787ab807ec657d8ff8f7aa9f41f68065ea9
ashokjung <ashokjung@gmail.com>
2020-08-24 23:36:38 +0530

    p Sliding Window Problem 1

--- src/main/java/com/ashok/array/MaxSubArray.java
+++ src/main/java/com/ashok/array/MaxSubArray.java
@@ -1,2 +1,27 @@
-package com.ashok.array;public class MaxSubArray {
+package com.ashok.array;
+
+public class MaxSubArray {
+
+    public static int findMaxSumSubArray(int k, int[] arr) {
+        int windowSum = 0, maxSum = 0;
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            windowSum += arr[windowEnd];
+            if (windowEnd >= k - 1) {
+                maxSum = Math.max(windowSum, maxSum);
+                windowSum -= arr[windowStart];
+                windowStart++;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum sum of a subarray of size K: "
+                + MaxSubArray.findMaxSumSubArray(3, new int[]{2, 1, 5, 1, 3, 2}));
+        System.out.println("Maximum sum of a subarray of size K: "
+                + MaxSubArray.findMaxSumSubArray(2, new int[]{2, 3, 4, 1, 5}));
+
+    }
 }
+
