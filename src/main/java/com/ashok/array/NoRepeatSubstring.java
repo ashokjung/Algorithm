f16d6cb9bd4fdaa064646e34477dce44b8e2b0c2
ashokjung <ashokjung@gmail.com>
2020-08-24 23:36:52 +0530

    p Sliding Window Problem 3

--- src/main/java/com/ashok/array/NoRepeatSubstring.java
+++ src/main/java/com/ashok/array/NoRepeatSubstring.java
@@ -1,2 +1,29 @@
-package com.ashok.array;public class NoRepeatSubstring {
+package com.ashok.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NoRepeatSubstring {
+
+    public static int findLength(String str) {
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> charIndexMap = new HashMap<>();
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            if (charIndexMap.containsKey(rightChar)) {
+                windowStart = Math.max(windowStart, charIndexMap.get(rightChar) + 1);
+            }
+            charIndexMap.put(rightChar, windowEnd);
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("aabccbb"));
+        System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abbbb"));
+        System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abccde"));
+
+    }
 }
